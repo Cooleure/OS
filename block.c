d9ebfd3a3d9caef00b8c6e6267a856a5cb80acc5
Florian-Dion <floflo.dion@free.fr>
2022-02-19 09:52:53 +0100

    Ajout des fichier couche 1 (.c et .h)

---
+++ block.c
@@ -0,0 +1,6 @@
+#include "block.h"
+
+int init_disk_sos (const char* dirname){
+    virtual_disk_t d0;
+    d0.
+}
---
+++ block.h
@@ -0,0 +1,66 @@
+/**
+ * @file block.h
+ * @author Florian Dion - Nicolas Dejean - Nathan Fleury
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-19
+ */
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include "struct.h"
+#define READ_FAILURE 404
+
+/**
+ * @brief Initialization of virtual_disk_sos and 
+ * returns 0 if Ok or 1 if KO
+ * 
+ * @param dirname 
+ * @return int 
+ */
+int init_disk_sos (const char* dirname);
+
+/**
+ * @brief save the catalog on the virtual disk
+ * 
+ * @return int 
+ */
+int switch_off ();
+
+/**
+ * @brief return the number of necessary block to stock size byte
+ * 
+ * @param size 
+ * @return int 
+ */
+int compute_nblock (const int size);
+
+/**
+ * @brief write the block b at the position pos on the disk
+ * 
+ * @param b 
+ * @param pos 
+ * @return int 
+ */
+int write_block (const block_t b, int pos);
+
+/**
+ * @brief Read the block "block" at the position pos and stock it in "b"
+ * return READ_FAILURE if there was a problem
+ * 
+ * @param block 
+ * @param pos 
+ * @param b 
+ * @return int 
+ */
+int read_block (const block_t block, int pos, block_t* b);
+
+/**
+ * @brief Print the block b in Hexadecimal on file name f
+ * 
+ * @param b 
+ * @param f 
+ */
+void print_block (const block_t b, char* f);
+
+#endif
