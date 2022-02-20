d18e1289ded9cc8d3241e76fd71d11e8fdf23c7b
MCAzertox <nathan.fleury@yahoo.fr>
2022-02-20 17:03:56 +0100

    Implementation of compute_nblock
    Ajout de la méthode permettant de calculer le nombre de blocs mémoire nécéssaire au stockage de "size" octets en mémoire.

--- block.c
+++ block.c
@@ -1,7 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 #include "block.h"
+#include "struct.h"
 
 virtual_disk_t vitual_disk_sos;
 
 void init_disk_sos (FILE* dirname){
   vitual_disk_sos.storage = dirname;
+}
+
+int compute_nblock(const int size){
+  return ceil(size/BLOCK_SIZE); //Arrondi par excès
 }
