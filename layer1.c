5c75fe2810727a46ef25ac507ef72cba3a3a339f
Florian-Dion <floflo.dion@free.fr>
2022-02-25 15:06:04 +0100

    change the write function and make the read function in layer 1

--- layer1.c
+++ layer1.c
@@ -29,6 +29,29 @@ int write_block (const block_t b, int pos){
     fprintf(stderr, "Probleme d'ecriture d'un bloc\n");
     return 1;
   }
-  close(fd);
+  if(close(fd) == -1){
+      fprintf(stderr, "Probleme de fermeture du disque");
+      return 1;
+  }
   return 0;
+}
+
+int read_block (block_t block, int pos){
+  int fd = open(virtual_disk_sos.storage, O_RDONLY);
+  if (fd == -1){
+    fprintf(stderr, "Probleme d'ouverture du disque\n");
+    return 1;
+  }
+
+  lseek(fd, pos*BLOCK_SIZE, SEEK_SET);
+  if (read(fd, &b, BLOCK_SIZE) != BLOCK_SIZE){
+    fprintf(stderr, "Probleme de lecture du block\n");
+    return READ_FAILURE;
+  }
+  if(close(fd) == -1){
+      fprintf(stderr, "Probleme de fermeture du disque");
+      return 1;
+  }
+  return 0;
+
 }
--- layer1.h
+++ layer1.h
@@ -1,7 +1,7 @@
 /**
  * @file layer1.h
  * @author Florian Dion - Nicolas Dejean - Nathan Fleury
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-02-19
  */
@@ -12,52 +12,51 @@
 #define READ_FAILURE 404
 
 /**
- * @brief Initialization of virtual_disk_sos and 
- * 
- * @param dirname  
+ * @brief Initialization of virtual_disk_sos and
+ *
+ * @param dirname
  */
 void init_disk_sos (const char* dirname);
 
 /**
  * @brief save the catalog on the virtual disk
- * 
- * @return int 
+ *
+ * @return int
  */
 int switch_off ();
 
 /**
  * @brief return the number of necessary block to stock size byte
- * 
- * @param size 
- * @return int 
+ *
+ * @param size
+ * @return int
  */
 int compute_nblock (const int size);
 
 /**
  * @brief write the block b at the position pos on the disk
- * 
- * @param b 
- * @param pos 
- * @return int 
+ *
+ * @param b
+ * @param pos
+ * @return int
  */
 int write_block (const block_t b, int pos);
 
 /**
- * @brief Read the block "block" at the position pos and stock it in "b"
+ * @brief Read the block "block" at the position pos in the disk
  * return READ_FAILURE if there was a problem
- * 
- * @param block 
- * @param pos 
- * @param b 
- * @return int 
+ *
+ * @param block
+ * @param pos
+ * @return int
  */
-int read_block (const block_t block, int pos, block_t* b);
+int read_block (block_t block, int pos);
 
 /**
  * @brief Print the block b in Hexadecimal on file name f
- * 
- * @param b 
- * @param f 
+ *
+ * @param b
+ * @param f
  */
 void print_block (const block_t b, char* f);
 
