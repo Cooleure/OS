2c52c953327b8ca75ea41019e8e8af3851ec3c51
Cooleure <nicolasdejean@laposte.net>
2022-04-05 19:16:32 +0200

    Layer 4

---
+++ layer4.c
@@ -0,0 +1,110 @@
+/**
+ * @file layer3.c
+ * @author Florian Dion - Nicolas Dejean - Nathan Fleury
+ * @brief
+ * @version 0.1
+ * @date 2022-04-05
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "layer4.h"
+#include "struct.h"
+
+int existing_file(inode_table_t *inode_table, char *filename) {
+	for (int i = 0; i < INODE_TABLE_SIZE; i++)
+		if (strcmp(inode_table[i] -> filename, filename) == 0)
+			return i; // Exists
+	return 0; // Does not exist
+}
+
+int write_file(char *filename, file_t *file) {
+	inode_table_t *inode_table;
+	if (read_inodes_table(inode_table)) {
+		int fileIndex = existing_file(inode_table, filename);
+		if (fileIndex) {
+			if (file -> size <= inode_table[fileIndex] -> size) {
+				// Modification de l'inode (à mettre dans une fonction update_inode() ?)
+				inode_table[fileIndex] -> size = file -> size;
+				inode_table[fileIndex] -> nblock = compute_nblock(file -> size);
+
+				time_t timing = time(NULL);
+  				strcpy(inode_table[fileIndex] -> mtimestamp, ctime(&timing));
+
+				//Ecriture de cette table mise à jour
+				int backup = write_inodes_table();
+				return backup;
+			} else {
+				delete_inode(fileIndex);
+				init_inode(filename, file -> size, virtual_disk_sos.super_block.first_free_byte);
+			}
+		}
+		else {
+			init_inode(filename, file -> size, virtual_disk_sos.super_block.first_free_byte);
+		}
+	} else {
+		fprintf(stderr, "Read inodes table problem\n");
+		return 0;
+	}
+
+	return 1;
+}
+
+int read_file(char *filename, file_t *file) {
+	inode_table_t *inode_table;
+	if (read_inodes_table(inode_table)) {
+		int fileIndex = existing_file(inode_table, filename);
+		if (fileIndex) {
+			// Lecture du fichier
+			fseek(virtual_disk_sos.storage, inode_table[fileIndex] -> first_byte, SEEK_SET);
+
+			if (fread(file -> data[MAX_FILE_SIZE], file -> size, 1, virtual_disk_sos.storage) != 1) {
+				fprintf(stderr, "Data file reading problem\n");
+				return READ_FAILURE;
+			}
+
+			file -> size = inode_table[fileIndex] -> size;
+		} else {
+			printf(stderr, "Attempt to read inexisting file\n");
+			return 0;
+		}
+	}
+	else {
+		fprintf(stderr, "Read inodes table problem\n");
+		return 0;
+	}
+
+	return 1;
+}
+
+int delete_file(char *filename) {
+	inode_table_t *inode_table;
+	if (read_inodes_table(inode_table)) {
+		int fileIndex = existing_file(inode_table, filename);
+		if (fileIndex) {
+			delete_inode(fileIndex);
+		} else {
+			fprintf(stderr, "Attempt to delete inexisting file\n");
+			return 0;
+		}
+	}
+	else {
+		fprintf(stderr, "Read inodes table problem\n");
+		return 0;
+	}
+
+	return 1;
+}
+
+int load_file_from_host(char *filename) {
+	return 1;
+}
+
+int store_file_to_host(char *filename) {
+	return 1;
+}
---
+++ layer4.h
@@ -0,0 +1,64 @@
+/**
+ * @file layer3.c
+ * @author Florian Dion - Nicolas Dejean - Nathan Fleury
+ * @brief
+ * @version 0.1
+ * @date 2022-04-05
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef LAYER4_H
+#define LAYER4_H
+
+#include "layer1.h"
+#include "layer2.h"
+#include "layer3.h"
+#include "struct.h"
+#include "sha256_utils.h"
+#include "sha256.h"
+
+/**
+ * @brief Write file on the system. If does not exist, then created, else updated.
+ *
+ * @param filename Name of the file
+ * @param file The file to read the data from
+ * @return 1 if no problems were encountered, 0 otherwise
+ */
+int write_file(char *filename, file_t *file);
+
+/**
+ * @brief Read the file from the system
+ *
+ * @param filename Name of the file
+ * @param file The file to write the result to
+ * @return 1 if no problems were encountered, 0 otherwise or if the file does not exist
+ */
+int read_file(char *filename, file_t *file);
+
+/**
+ * @brief Delete the file from system
+ *
+ * @param filename Name of the file
+ * @return 1 if no problems were encountered, 0 otherwise of if the file does not exist
+ */
+int delete_file(char *filename);
+
+/**
+ * @brief Read the file from the host computer and write it into the system
+ *
+ * @param filename Name of the file
+ * @return 1 if no problems were encountered, 0 otherwise
+ */
+int load_file_from_host(char *filename);
+
+/**
+ * @brief Read the file from the system and write it into the host computer
+ *
+ * @param filename Name of the file
+ * @return 1 if no problems were encountered, 0 otherwise
+ */
+int store_file_to_host(char *filename);
+
+#endif
